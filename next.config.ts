7275f59360ab9c438a65eab2bd33df58c41fbadb
Rajasree460 <laharajasree121@gmail.com>
2025-04-22 12:04:12 +0530

    fixed all

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // devIndicators: {
+  //   buildActivity: false, //  disables the floating "N" icon
+  // },
   
 };
 
